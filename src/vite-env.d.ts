441d12b9759326b01abfc44083583b864e603f10
itayalasas <info@ayalait.com.uy>
2025-11-07 11:45:28 -0300

    Updated vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,6 +7,7 @@ interface ImportMetaEnv {
   readonly VITE_AUTH_APP_ID: string;
   readonly VITE_AUTH_API_KEY: string;
   readonly VITE_REDIRECT_URI: string;
+  readonly AUTH_VALIDA_TOKEN?: string;
 }
 
 interface ImportMeta {
